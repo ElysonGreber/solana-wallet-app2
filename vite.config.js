c3a50df27fda13f9dffe14cdc4e4407220529254
BlackBloc <49597405+ElysonGreber@users.noreply.github.com>
2025-06-21 07:40:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/solana-wallet-app/',
+  base: '/solana-wallet-app2/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
